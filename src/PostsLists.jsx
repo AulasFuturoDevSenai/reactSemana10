4e38f2b1c4cb9811a7e0c88c78827bc5a6349d4f
Emanuele Magalhães <130506951+EmanueleMagalhaes@users.noreply.github.com>
2025-08-02 18:54:26 -0300

    Tornei o componente dinâmico com props

--- src/PostsLists.jsx
+++ src/PostsLists.jsx
@@ -1,7 +1,23 @@
 
-
+import Post from "./Post";
 
 function PostsLists() {
+    return (
+        <div>
+            <Post tipo='NOTÍCIA' 
+                titulo='5 Tendências Tech para 2026'
+                descricao="De computação quântica ao metaverso corporativo, conheça o que vem por aí."
+                data='09/07/2025'
+                />
+
+            <Post
+                tipo="ARTIGO"
+                titulo="Inteligência Artificial no Dia a Dia"
+                descricao="Como a IA está revolucionando serviços e impactando decisões em empresas e governos."
+                data="14/07/2025"
+            />
+        </div>
+    );
    
 }
 
